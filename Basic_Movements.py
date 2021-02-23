b8924cc4b2064f4e80a2b8ab5a393f7c3f0b26df
Saif Mustafa <68763259+SMGLOBAL-ops@users.noreply.github.com>
2021-02-23 16:38:02 +0000

    Create Basic_Movements.py

---
+++ Basic_Movements.py
@@ -0,0 +1,20 @@
+from djitellopy import tello
+from time import sleep
+
+
+me = tello.Tello()
+me.connect()
+print(me.get_battery())
+
+# General commands for movement of the drone
+
+me.takeoff()
+me.send_rc_control(0, 50, 0, 0)
+sleep(2)
+me.send_rc_control(0, 0, 0, 0)
+sleep(2)
+me.send_rc_control(0, 30, 0, 0)
+me.land()
+
+
+
