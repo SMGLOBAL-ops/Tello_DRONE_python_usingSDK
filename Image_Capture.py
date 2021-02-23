a04cf01d69c6d53dffa044f118df1efd6e46bb9e
Saif Mustafa <68763259+SMGLOBAL-ops@users.noreply.github.com>
2021-02-23 16:38:25 +0000

    Create Image_Capture.py

---
+++ Image_Capture.py
@@ -0,0 +1,20 @@
+from djitellopy import tello
+from time import sleep
+import cv2
+
+me = tello.Tello()
+me.connect()
+print(me.get_battery())
+
+me.stream_on()
+
+# Get image from Tello drone
+
+while True:
+        img = me.get_frame_read().frame
+        img = cv2.resize(img, (360, 240))
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+
